53592e540a7327dd210ebc4ff768212dd7335495
CRMDynamics365BCPowerApps <CRMDynamics365BCPowerApps>
2025-11-06 02:41:18 -0600

    Add user roles, JWT auth, Register/Login DTOs and migration First01

--- EmployeeManagementSystemSolution/Server/Program.cs
+++ EmployeeManagementSystemSolution/Server/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using ServerLibrary.Data;
+using ServerLibrary.Helpers;
+using ServerLibrary.Repositories.Contracts;
+using ServerLibrary.Repositories.Implementations;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +21,8 @@ builder.Services.AddDbContext<AppDbContext>(options => {
             throw new InvalidOperationException("Sorry, your connection is not found"));
 });
 
-
+builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
+builder.Services.AddScoped<IUserAccount, UserAccountRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ EmployeeManagementSystemSolution/ServerLibrary/Repositories/Contracts/IUserAccount.cs
@@ -0,0 +1,13 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using ServiceStack;
+using Register = BaseLibrary.DTOs.Register;
+namespace ServerLibrary.Repositories.Contracts
+{
+    public interface IUserAccount
+    {
+        Task<GeneralResponse> CreateAsync(Register user);
+        Task<LoginResponse> SignInAsync(Login user);
+
+    }
+}
